0664b8f4ff09d3b5be05a6ae8a5ed28f46b287ac
samar08 <samarasimhareddy.mohan@gmail.com>
2021-02-13 01:34:09 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Board from "./components/Board";
+import "./styles/root.scss";
 export default () => (
   <>
+  <div className="app">
     <h1>TIC TAC TOE</h1>
     <Board/>
+    </div>
   </>
 );
