b936c4df2ee2ef8a6c8835c41e4fa0ca0b999e68
samar08 <samarasimhareddy.mohan@gmail.com>
2021-04-02 10:21:23 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
 import Board from "./components/Board";
+import Foot from "./components/Foot";
+
 import "./styles/root.scss";
-export default () => (
-  <>
+function App(){
+  const [board,setBoard]=useState(Array(9).fill(null));
+  const [isXnext,setisXnext]=useState(false);
+  const [player_id,set_player_id]=useState(1);
+//const [message,setmessage]=useState("Player "+player_id+"'s turn");
+  function handleSquareClick(position){
+     // console.log(board[position]);
+     if(board[position]==null){
+      setBoard((prev)=>{
+       return prev.map((square,pos)=>{
+           if(pos===position){
+               return isXnext? 'X': 'O';
+              
+           }
+           return square;
+       });
+      });
+      setisXnext((prev)=>!prev);
+      set_player_id(
+        
+        (previd)=>{
+          console.log(previd);
+        if(previd===1)
+        {
+          return 2;
+        }
+        else{
+          return 1;
+        }
+        console.log(previd);
+      });
+      console.log(player_id);
+      //setmessage(()=>{return ("Player "+player_id+"'s turn");})
+  }
+  else{
+      return;
+  }
+  }
+  return(
+  
   <div className="app">
     <h1>TIC TAC TOE</h1>
-    <Board/>
+    <h2>Player {player_id}'s turn</h2>
+   
+    <Board board={board} handleSquareClick={handleSquareClick}/>
+   
+    <Foot className="footer"/>
     </div>
-  </>
-);
+   
+  );
+};
+export default App;
---
+++ src/components/Foot.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Emoji from 'a11y-react-emoji'
+function Foot(){
+  
+    return (
+        <footer>
+        Made with
+        {' '}
+        <Emoji symbol="💕" label="love" />
+        {' '}
+        by Samar
+    </footer>
+    
+    )
+
+}
+
+export default Foot;
